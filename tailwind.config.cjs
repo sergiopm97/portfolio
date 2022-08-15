9d2db0da5375527e4a65fa62c48ca282b66a9a9d
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-15 15:02:53 +0200

    Add style variables and body background
    Adding global style variables to the Tailwind CSS theme and defining a custom background color for the body of the application

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "std-green": "#64ffda",
+        "std-navy": "#0a192f",
+        "light-navy": "#233554",
+        "std-slate": "#8892b0",
+        "light-slate": "#ccd6f6",
+        "std-white": "#e6f1ff",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        spaceMono: ["Space Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
