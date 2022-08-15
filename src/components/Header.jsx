17d269b1d06943859dfa3ef1d6e68c93b3326283
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-15 16:12:32 +0200

    Reposition properties of the component logo
    Transfer of the properties of the logo component to the interior of the component itself

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,13 @@
 import Logo from "./Logo";
-import fetcher from "../services/fetcher";
-import { getData } from "../services/getData";
-import { config } from "../../config";
 
 function Header() {
-  const authorEndpoint = config.apiEndpoints.author;
-  const authorData = getData(authorEndpoint, fetcher);
-
-  if (authorData) {
-    return (
-      <header className="w-full h-24 flex items-center justify-center">
-        <div className="max-w-screen-2xl w-11/12 h-4/5 flex items-center justify-between">
-          <Logo authorData={authorData} />
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="w-full h-24 flex items-center justify-center">
+      <div className="max-w-screen-2xl w-11/12 h-4/5 flex items-center justify-between">
+        <Logo />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,18 @@
-function Logo(props) {
-  return (
-    <div className="w-12 h-12 flex items-center justify-center border-2 border-std-green rounded-full cursor-pointer hover:bg-std-green/[0.2] transition-colors">
-      <h1 className="font-spaceMono text-std-green text-xl">{props.authorData.first_name[0]}</h1>
-    </div>
-  );
+import fetcher from "../services/fetcher";
+import { getData } from "../services/getData";
+import { config } from "../../config";
+
+function Logo() {
+  const authorEndpoint = config.apiEndpoints.author;
+  const authorData = getData(authorEndpoint, fetcher);
+
+  if (authorData) {
+    return (
+      <div className="w-12 h-12 flex items-center justify-center border-2 border-std-green rounded-full cursor-pointer hover:bg-std-green/[0.2] transition-colors">
+        <h1 className="font-spaceMono text-std-green text-xl">{authorData.first_name[0]}</h1>
+      </div>
+    );
+  }
 }
 
 export default Logo;
