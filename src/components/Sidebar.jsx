740d1d3682e123be429b4a4675ff5f0e7fcb5312
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-17 17:22:28 +0200

    Implement responsive and animations to the sidebar
    Implementation of the responsive sidebar making it not visible from 1024px downwards and adding an entry animation to it

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import fetcher from "../services/fetcher";
 import { getData } from "../services/getData";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { motion } from "framer-motion";
 import { config } from "../../config";
 
 function Sidebar() {
@@ -20,15 +21,22 @@ function Sidebar() {
     });
 
     return (
-      <div className="w-12 h-[calc(100vh-6rem)] flex flex-col items-center justify-end fixed">
-        <a href={githubLink} target={"_blank"}>
-          <FiGithub className="w-6 h-6 cursor-pointer fill-0 stroke-std-slate hover:stroke-std-green hover:-translate-y-1 transition-all" />
-        </a>
-        <a href={linkedinLink} target={"_blank"}>
-          <FiLinkedin className="w-6 h-6 mt-6 cursor-pointer fill-0 stroke-std-slate hover:stroke-std-green hover:-translate-y-1 transition-all" />
-        </a>
+      <motion.div
+        className="hidden lg:flex w-20 h-[calc(100vh-6rem)] flex-col items-center justify-end fixed"
+        initial={{ y: "50%", opacity: 0 }}
+        animate={{ y: "0%", opacity: 1 }}
+        transition={{ duration: 1.5, delay: 0.25 }}
+      >
+        <div className="flex flex-col gap-5">
+          <a href={githubLink} target={"_blank"}>
+            <FiGithub className="w-6 h-6 cursor-pointer fill-0 stroke-std-slate hover:stroke-std-green hover:-translate-y-1 transition-all" />
+          </a>
+          <a href={linkedinLink} target={"_blank"}>
+            <FiLinkedin className="w-6 h-6 cursor-pointer fill-0 stroke-std-slate hover:stroke-std-green hover:-translate-y-1 transition-all" />
+          </a>
+        </div>
         <div className="w-0.5 h-52 mt-12 bg-std-slate"></div>
-      </div>
+      </motion.div>
     );
   }
 }
