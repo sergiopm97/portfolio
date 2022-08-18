04f2fd3ca0384dd22a113b01413a168b3767b701
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-18 15:32:32 +0200

    Add CV download
    Addition in the resume button the functionality of being able to download the author's CV by clicking on it

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const config = {
     author: "https://1vze7rmf.directus.app/items/author",
     navigation: "https://1vze7rmf.directus.app/items/navigation",
     social: "https://1vze7rmf.directus.app/items/social",
+    resume: "https://1vze7rmf.directus.app/assets/347ff453-47f8-4ab0-900e-4e741d58002c?download",
   },
 };
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import { config } from "../../config";
+
 function About() {
   return (
     <div className="w-full h-[calc(100vh-6rem)] flex items-center justify-between lg:pl-48 lg:pr-28">
@@ -9,9 +11,11 @@ function About() {
           I am a developer specialized in data analysis and data processing, AI creation and currently growing in
           the web development world with a strong background in frontend but with basic knowledge in backend.
         </p>
-        <button className="xl:text-base px-6 py-3 rounded text-std-green bg-dark-navy/[0.5] font-semibold hover:bg-dark-navy transition-colors">
-          Check my resume
-        </button>
+        <a href={config.apiEndpoints.resume}>
+          <button className="xl:text-base px-6 py-3 rounded text-std-green bg-dark-navy/[0.5] font-semibold hover:bg-dark-navy transition-colors">
+            Check my resume
+          </button>
+        </a>
       </div>
       <div className="w-5/12 flex justify-end">
         <img src="src/assets/programmer.gif"></img>
