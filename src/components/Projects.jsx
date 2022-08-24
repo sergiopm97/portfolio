4a56df5d66884a6d943bb4bffaf65fd7603ba00c
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-24 15:30:08 +0200

    Add scroll animations
    Implementation of animations in the projects component and in the blocks that compose it when they become visible on the screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import Project from "./Project";
 import Slider from "react-slick";
 import fetcher from "../services/fetcher";
 import { getData } from "../services/getData";
+import { motion } from "framer-motion";
 import { config } from "../../config";
 
 import "slick-carousel/slick/slick.css";
@@ -39,17 +40,30 @@ function Projects() {
         className="w-full pt-32 mt-[-8rem] lg:pl-40 md:pl-16 sm:pl-10 xs:pl-8 pl-5 lg:pr-20 md:pr-16 sm:pr-10 xs:pr-8 pr-5"
         id="projects"
       >
-        <div className="xl:text-4xl lg:text-3xl xs:text-4xl text-3xl xs:mb-16 mb-10">
+        <motion.div
+          className="xl:text-4xl lg:text-3xl xs:text-4xl text-3xl xs:mb-16 mb-10"
+          initial={{ y: "50%", opacity: 0 }}
+          whileInView={{ y: "0%", opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
           <h1 className="text-std-white font-semibold">
             <span className="mr-2 text-std-green font-spaceMono font-normal">2.</span>
             Proyects
           </h1>
-        </div>
-        <Slider {...sliderSettings}>
-          <Project data={projectsData} project={"Sokkai"} />
-          <Project data={projectsData} project={"Samba"} />
-          <Project data={projectsData} project={"Portfolio"} />
-        </Slider>
+        </motion.div>
+        <motion.div
+          initial={{ y: "50%", opacity: 0 }}
+          whileInView={{ y: "0%", opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
+          <Slider {...sliderSettings}>
+            <Project data={projectsData} project={"Sokkai"} />
+            <Project data={projectsData} project={"Samba"} />
+            <Project data={projectsData} project={"Portfolio"} />
+          </Slider>
+        </motion.div>
       </div>
     );
   }
