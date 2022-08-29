c0f955ba754f62cb2114d0813ac3473f82e38814
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-08-29 18:44:38 +0200

    Add links to social icons
    Implementation of the links corresponding to each of the social network icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,51 @@
+import fetcher from "../services/fetcher";
+import { getData } from "../services/getData";
 import { AiOutlineCopyrightCircle } from "react-icons/ai";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import { motion } from "framer-motion";
+import { config } from "../../config";
 
 function Footer() {
-  return (
-    <motion.footer
-      className="w-full pt-32 mt-[-8rem] mb-10 lg:pl-40 md:pl-16 sm:pl-10 xs:pl-8 pl-5 lg:pr-20 md:pr-16 sm:pr-10 xs:pr-8 pr-5"
-      initial={{ y: "50%", opacity: 0 }}
-      whileInView={{ y: "0%", opacity: 1 }}
-      viewport={{ once: true }}
-      transition={{ duration: 1 }}
-    >
-      <div className="xs:h-16 flex xs:flex-row flex-col items-center justify-center lg:gap-10 gap-6 rounded-3xl xs:py-0 py-5 bg-dark-navy">
-        <h1 className="text-lg text-std-slate font-roboto font-semibold">By Sergio Peña</h1>
-        <h1 className="flex items-center gap-2 text-lg text-std-slate font-roboto font-semibold">
-          Copyright 2022 <AiOutlineCopyrightCircle />
-        </h1>
-        <div className="lg:hidden flex items-center gap-3 text-std-slate">
-          <FiGithub className="w-5 h-5" />
-          <FiLinkedin className="w-5 h-5" />
+  const socialEndpoint = config.apiEndpoints.social;
+  const socialData = getData(socialEndpoint, fetcher);
+
+  if (socialData) {
+    let githubLink = new String();
+    let linkedinLink = new String();
+
+    socialData.map((socialItem) => {
+      if (socialItem["Name"] == "GitHub") {
+        githubLink = socialItem["Url"];
+      } else if (socialItem["Name"] == "LinkedIn") {
+        linkedinLink = socialItem["Url"];
+      }
+    });
+
+    return (
+      <motion.footer
+        className="w-full pt-32 mt-[-8rem] mb-10 lg:pl-40 md:pl-16 sm:pl-10 xs:pl-8 pl-5 lg:pr-20 md:pr-16 sm:pr-10 xs:pr-8 pr-5"
+        initial={{ y: "50%", opacity: 0 }}
+        whileInView={{ y: "0%", opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+        <div className="xs:h-16 flex xs:flex-row flex-col items-center justify-center lg:gap-10 gap-6 rounded-3xl xs:py-0 py-5 bg-dark-navy">
+          <h1 className="text-lg text-std-slate font-roboto font-semibold">By Sergio Peña</h1>
+          <h1 className="flex items-center gap-2 text-lg text-std-slate font-roboto font-semibold">
+            Copyright 2022 <AiOutlineCopyrightCircle />
+          </h1>
+          <div className="lg:hidden flex items-center gap-3 text-std-slate">
+            <a href={githubLink} target={"_blank"}>
+              <FiGithub className="w-5 h-5 cursor-pointer hover:stroke-std-green hover:-translate-y-1 transition-all" />
+            </a>
+            <a href={linkedinLink} target={"_blank"}>
+              <FiLinkedin className="w-5 h-5 cursor-pointer hover:stroke-std-green hover:-translate-y-1 transition-all" />
+            </a>
+          </div>
         </div>
-      </div>
-    </motion.footer>
-  );
+      </motion.footer>
+    );
+  }
 }
 
 export default Footer;
